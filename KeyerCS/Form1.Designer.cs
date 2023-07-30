b9cb505952d264d826832feab51d1b050340d788
DKopeikin <DKopeikin@PP-WS-UNITY>
2023-07-31 02:10:04 +0300

    first commit

---
+++ KeyerCS/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace KeyerCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.bBrowse = new System.Windows.Forms.Button();
+            this.cd = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbPath.Location = new System.Drawing.Point(12, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(206, 26);
+            this.tbPath.TabIndex = 0;
+            // 
+            // bBrowse
+            // 
+            this.bBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bBrowse.Location = new System.Drawing.Point(12, 44);
+            this.bBrowse.Name = "bBrowse";
+            this.bBrowse.Size = new System.Drawing.Size(87, 37);
+            this.bBrowse.TabIndex = 1;
+            this.bBrowse.Text = "Browse";
+            this.bBrowse.UseVisualStyleBackColor = true;
+            this.bBrowse.Click += new System.EventHandler(this.bBrowse_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "extract";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 176);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bBrowse);
+            this.Controls.Add(this.tbPath);
+            this.Name = "Form1";
+            this.Text = "Keyer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button bBrowse;
+        private System.Windows.Forms.ColorDialog cd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog ofd;
+    }
+}
+
---
+++ KeyerCS/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyerCS
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bBrowse_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = ofd.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap input = new Bitmap(tbPath.Text);
+            Bitmap output = new Bitmap(input.Width, input.Height);
+            for (int y = 0; y < output.Height; y++) {
+                for (int x = 0; x < output.Width; x++) {
+                    Color color = input.GetPixel(x, y);
+                    byte max = Math.Max(Math.Max(color.R, color.G), color.B);
+                    byte min = Math.Min(Math.Min(color.R, color.G), color.B);
+                    if (color.G != min && (color.G == max || max - color.G < 10) && (max - min) > 50) {
+                        output.SetPixel(x, y, Color.Transparent);
+                    }
+                    else {
+                        output.SetPixel(x, y, color);
+                    }
+                }
+            }
+            Image img = output;
+            img.Save(tbPath.Text.Substring(0, tbPath.Text.LastIndexOf('.')) + "_RESULT.png");
+        }
+
+        private Color CustomColor(int r, int g, int b, int a = 255)
+        {
+            Color color = Color.FromArgb(a, r, g, b);
+            return color;
+        }
+    }
+}
